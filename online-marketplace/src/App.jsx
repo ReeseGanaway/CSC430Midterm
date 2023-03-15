56872181f1885cc07862ff469f1700b79621f295
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2023-03-15 16:18:06 -0400

    added a component for product list when you click on product on landing page

--- online-marketplace/src/App.jsx
+++ online-marketplace/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import { Rating } from "@mui/material";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import ProductList from "./components/ProductList";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,9 +13,10 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route element={<LandingPage />} >
+            
               <Route path="/" element={<LandingPage />} />
-            </Route>
+              <Route path="/productlist" element={<ProductList />} />
+           
           </Routes>
         </BrowserRouter>
       </div>
