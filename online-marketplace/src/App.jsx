b7c3d5f7adf2c0ce95c975ac704764e068f1c983
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2023-03-15 16:03:41 -0400

    Set up react router

--- online-marketplace/src/App.jsx
+++ online-marketplace/src/App.jsx
@@ -1,42 +1,24 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { Rating } from "@mui/material";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   const [count, setCount] = useState(0);
   const [value, setValue] = useState(2);
 
   return (
-    <div className="bg-slate-300">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route element={<LandingPage />} >
+              <Route path="/" element={<LandingPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        precision={0.5}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      Stars: {value}/5
-    </div>
-  );
+    );
 }
 
 export default App;
