48d5b365f55d2fa2eb434eac433fbc353d53e794
Qumber Zaidi <Qumber.Zaidi@cix.csi.cuny.edu>
2023-03-24 12:56:59 -0400

    signup/login page commit

--- online-marketplace/src/App.jsx
+++ online-marketplace/src/App.jsx
@@ -1,40 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Rating } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState(2);
+import FormSwitch from "./components/FormSwitch";
+
+const App = () => {
 
   return (
-    <div className="bg-slate-300">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
-      
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-    </div>
+   
+   <>
+<FormSwitch></FormSwitch>
+
+   </>
   );
 }
 
---
+++ online-marketplace/src/components/Background.jsx
@@ -0,0 +1,25 @@
+import './Background.css'
+const Background = () => {
+
+
+return (
+
+<ul class="background">
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+</ul>
+)};
+
+export default Background;
