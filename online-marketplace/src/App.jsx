b271214c750db4c859b27a19e625db1ebe01eec5
BolaGhaly <bolaghaly63@gmail.com>
2023-03-24 19:36:20 -0400

    Use navbar as a layout

--- online-marketplace/src/App.jsx
+++ online-marketplace/src/App.jsx
@@ -1,17 +1,30 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import FormSwitch from "./components/FormSwitch";
+import Sidebar from "./components/Sidebar";
+import { styled } from "@mui/material/styles";
 
 function App() {
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  }));
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<FormSwitch />} />
-        </Routes>
-      </BrowserRouter>
+      <Sidebar />
+      <DrawerHeader />
+      <main className="ml-16 p-6">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<FormSwitch />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
