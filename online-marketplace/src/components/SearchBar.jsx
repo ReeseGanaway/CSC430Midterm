637d3c7823177d98f25e85afe656f2cf4d8864c7
BolaGhaly <bolaghaly63@gmail.com>
2023-03-25 19:45:31 -0400

    Update modal

--- online-marketplace/src/components/SearchBar.jsx
+++ online-marketplace/src/components/SearchBar.jsx
@@ -5,8 +5,9 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Rating from "@mui/material/Rating";
 import Modal from "@mui/material/Modal";
+import { CgClose } from "react-icons/cg";
 
-function SearchBar() {
+const SearchBar = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedProduct, setSelectedProduct] = useState(null);
@@ -88,7 +89,10 @@ function SearchBar() {
               />
               <span className="pl-1">({product.rating.count})</span>
             </p>
-            <button className="flex justify-center items-end" onClick={() => handleProductClick(product)}>
+            <button
+              className="flex justify-center items-end"
+              onClick={() => handleProductClick(product)}
+            >
               <a
                 href="#_"
                 className="relative inline-flex items-center justify-center my-3 px-5 py-2 overflow-hidden font-medium text-blue-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group"
@@ -131,18 +135,7 @@ function SearchBar() {
               <div className="flex justify-between items-center pb-3 border-b-2">
                 <h1 className="text-lg font-bold">{selectedProduct.title}</h1>
                 <button onClick={handleCloseModal}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-gray-500 hover:text-gray-700 transition-colors duration-200"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 9.172l3.536-3.536a1 1 0 011.414 0l.707.707a1 1 0 010 1.414L12.12 10l3.536 3.536a1 1 0 01-1.414 1.414L10 11.88l-3.536 3.536a1 1 0 01-1.414-1.414L7.88 10 4.344 6.464a1 1 0 010-1.414l.707-.707a1 1 0 011.414 0L10 9.172z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <CgClose className="text-red-500 text-xl stroke-1" />
                 </button>
               </div>
               <div className="flex justify-between pt-3">
@@ -152,18 +145,16 @@ function SearchBar() {
                   className="w-52 h-52 object-scale-down mr-5"
                 />
                 <div>
-                  <p className="text-gray-700 font-semibold mb-2">
-                  <strong>Price: </strong>
-                    ${selectedProduct.price.toFixed(2)}
-                  </p>
                   <p className="text-gray-700 mb-2">
-                  <strong>Description: </strong> 
+                    <strong>Description: </strong>
                     {selectedProduct.description}
                   </p>
+                  <p className="text-gray-700 mb-2">
+                    <strong>Price: </strong>${selectedProduct.price.toFixed(2)}
+                  </p>
                   <div className="flex items-center">
-                    <span className="text-gray-700 font-semibold pr-2">
-                      Rating:
-                    </span>
+                    <strong className="text-gray-700 pr-1">Rating:</strong>
+                    <span className="pr-1">{selectedProduct.rating.rate}</span>
                     <Rating
                       name="read-only"
                       value={selectedProduct.rating.rate}
@@ -183,5 +174,5 @@ function SearchBar() {
       )}
     </>
   );
-}
+};
 export default SearchBar;
