185c4cdd71b2b496828196563d3fd7e3cd60473e
BolaGhaly <bolaghaly63@gmail.com>
2023-03-24 20:06:38 -0400

    Update landing page

---
+++ online-marketplace/src/components/ProductsList.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default ProductsList;
